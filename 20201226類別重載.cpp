71c2b7ccfe1184e59c974d4b3ea6309377c48ff2
chenyanda1222 <313027@s.lhvs.tyc.edu.tw>
2024-12-26 14:45:25 +0800

    Update 20201226類別重載.cpp

--- 20201226類別重載.cpp
+++ 20201226類別重載.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class myTime{
+	private:
+		int hour;
+		int minute;
+	public:
+		myTime();
+		myTime(int h,int m=30);
+		void setTime(int h,int m);
+		void printTime();		
+}; 
+myTime::myTime(){
+	hour = 9; minute = 0;
+}
+myTime::myTime(int h,int m){
+	setTime(h,m);
+}
+void myTime::setTime(int h,int m){
+	hour=h;
+	minute=m;
+}
+void myTime::printTime(){
+	cout<<hour<<":"<<minute<<"\n";
+}
+
+main(){
+	
+}
+
 
